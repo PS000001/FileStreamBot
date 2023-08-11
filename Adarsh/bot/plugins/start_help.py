11239337773ceb4d34befa7fcd7701f8e41b2987
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-08-11 06:25:39 -0700

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -1,4 +1,3 @@
-# (c) adarsh-goel 
 from Adarsh.bot import StreamBot
 from Adarsh.vars import Var
 import logging
@@ -6,63 +5,59 @@ logger = logging.getLogger(__name__)
 from Adarsh.bot.plugins.stream import MY_PASS
 from Adarsh.utils.human_readable import humanbytes
 from Adarsh.utils.database import Database
-from pyrogram import filters, ReplyKeyboardMarkup,
+from pyrogram import filters, ReplyKeyboardMarkup
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant
 from Adarsh.utils.file_properties import get_name, get_hash, get_media_file_size
+
 db = Database(Var.DATABASE_URL, Var.name)
-from pyrogram.types import ReplyKeyboardMarkup
 
 button_main_channel = InlineKeyboardButton("Main Channel", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
 button_owner = InlineKeyboardButton("Owner", url="https://t.me/MadAsGhost")
-button_status = InlineKeyboardButton("Status", callback_data="check_status")
+button_support_channel = InlineKeyboardButton("Join Support Channel", url=f"https://t.me/{Var.SUPPORT_CHANNEL}")
 
 buttons = [
     [button_main_channel],
-    [button_owner, button_status]
+    [button_owner]
 ]
 
-
-@StreamBot.on_message((filters.command("start") | filters.regex('start⚡️')) & filters.private )
+@StreamBot.on_message((filters.command("start")) & filters.private)
 async def start(b, m):
     if not await db.is_user_exist(m.from_user.id):
         await db.add_user(m.from_user.id)
         await b.send_message(
             Var.BIN_CHANNEL,
-            f"**Nᴇᴡ Usᴇʀ Jᴏɪɴᴇᴅ:** \n\n__Mʏ Nᴇᴡ Fʀɪᴇɴᴅ__ [{m.from_user.first_name}](tg://user?id={m.from_user.id}) __Sᴛᴀʀᴛᴇᴅ Yᴏᴜʀ Bᴏᴛ !!__"
+            f"**New User Joined:** \n\n__My New Friend__ [{m.from_user.first_name}](tg://user?id={m.from_user.id}) __Started Your Bot!!__"
         )
-    if Var.UPDATES_CHANNEL != "None":
-        try:
-            user = await b.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
-            if user.status == "kicked":
-                await b.send_message(
-                    chat_id=m.chat.id,
-                    text="__Sorry, You Are Banned Using Me. Contact Developer__\n\n  **He Will Help You**",
-                    disable_web_page_p𝓞𝓤 𝓐𝓡𝓔 𝓐𝓡𝓔 𝓑𝓐𝓝𝓝𝓔𝓓review=True
-                )
-                return
-        except UserNotParticipant:
-           await bot.send_photo(
-                chat_id=message.chat.id,
-                photo="https://graph.org/file/d454b953103d42d759f8d.jpg",
-                caption="Join our main channel to use this bot!",
-                reply_markup=InlineKeyboardMarkup(buttons)
-            )
-            return
-        except Exception:
+
+    try:
+        user = await b.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
+        if user.status == "kicked":
             await b.send_message(
                 chat_id=m.chat.id,
-                text="<i>Somthing Went Wrong</i> <b> <a href='https://t.me/MadAsGhost'>CLICK HERE FOR SUPPORT </a></b>",
-                
-                disable_web_page_preview=True)
+                text="__Sorry, You Are Banned Using Me. Contact Developer__\n\n  **He Will Help You**",
+                disable_web_page_preview=True
+            )
             return
-    await StreamBot.send_photo(
-        chat_id=message.chat.id,
-        photo="https://graph.org/file/d454b953103d42d759f8d.jpg",
-        caption=f'Hi {message.from_user.mention(style="md")}!,\nI am Telegram File to Link Generator Bot with Channel support.\nSend me any file and get a direct download link and streamable link.!',
-        reply_markup=ReplyKeyboardMarkup(buttons, resize_keyboard=True)
-    )
+    except UserNotParticipant:
+        await b.send_photo(
+            chat_id=m.chat.id,
+            photo="https://graph.org/file/d454b953103d42d759f8d.jpg",
+            text="To access the bot's features, please join the support channel by clicking the button below:",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [button_support_channel]
+                ]
+            )
+        )
+        return
 
+    await b.send_message(
+        chat_id=m.chat.id,
+        text="""<b> Send me any file or video i will give you streamable link and download link.</b>\n
+<b> I also support Channels, add me to you Channel and send any media files and see miracle✨ also send /list to know all commands""",
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
 
 @StreamBot.on_message((filters.command("help") | filters.regex('help📚')) & filters.private )
 async def help_handler(bot, message):
@@ -70,49 +65,34 @@ async def help_handler(bot, message):
         await db.add_user(message.from_user.id)
         await bot.send_message(
             Var.BIN_CHANNEL,
-            f"**Nᴇᴡ Usᴇʀ Jᴏɪɴᴇᴅ **\n\n__Mʏ Nᴇᴡ Fʀɪᴇɴᴅ__ [{message.from_user.first_name}](tg://user?id={message.from_user.id}) __Started Your Bot !!__"
+            f"**New User Joined:** \n\n__My New Friend__ [{message.from_user.first_name}](tg://user?id={message.from_user.id}) __Started Your Bot!!__"
         )
-    if Var.UPDATES_CHANNEL != "None":
-        try:
-            user = await bot.get_chat_member(Var.UPDATES_CHANNEL, message.chat.id)
-            if user.status == "kicked":
-                await bot.send_message(
-                    chat_id=message.chat.id,
-                    text="<i>Sᴏʀʀʏ Sɪʀ, Yᴏᴜ ᴀʀᴇ Bᴀɴɴᴇᴅ FROM USING ᴍᴇ. Cᴏɴᴛᴀᴄᴛ ᴛʜᴇ Dᴇᴠᴇʟᴏᴘᴇʀ</i>",
-                    
-                    disable_web_page_preview=True
-                )
-                return
-        except UserNotParticipant:
-            await StreamBot.send_photo(
-                chat_id=message.chat.id,
-                photo="https://graph.org/file/d454b953103d42d759f8d.jpg",
-                Caption="**𝙹𝙾𝙸𝙽 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙶𝚁𝙾𝚄𝙿 𝚃𝙾 𝚄𝚂𝙴 ᴛʜɪs Bᴏᴛ!**\n\n__Dᴜᴇ ᴛᴏ Oᴠᴇʀʟᴏᴀᴅ, Oɴʟʏ Cʜᴀɴɴᴇʟ Sᴜʙsᴄʀɪʙᴇʀs ᴄᴀɴ ᴜsᴇ ᴛʜᴇ Bᴏᴛ!__",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("🤖 Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
-                        ]
-                    ]
-                ),
-                
-            )
-            return
-        except Exception:
+
+    try:
+        user = await bot.get_chat_member(Var.UPDATES_CHANNEL, message.chat.id)
+        if user.status == "kicked":
             await bot.send_message(
                 chat_id=message.chat.id,
-                text="__Sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ Wʀᴏɴɢ. Cᴏɴᴛᴀᴄᴛ ᴍᴇ__ [Mikey](https://t.me/MadAsGhost).",
-                disable_web_page_preview=True)
+                text="__Sorry, You Are Banned Using Me. Contact Developer__\n\n  **He Will Help You**",
+                disable_web_page_preview=True
+            )
             return
-    await message.reply_text(
+    except UserNotParticipant:
+        await bot.send_photo(
+            chat_id=message.chat.id,
+            photo="https://graph.org/file/d454b953103d42d759f8d.jpg",
+            text="To access the bot's features, please join the support channel by clicking the button below:",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [button_support_channel]
+                ]
+            )
+        )
+        return
+
+    await bot.send_message(
+        chat_id=message.chat.id,
         text="""<b> Send me any file or video i will give you streamable link and download link.</b>\n
 <b> I also support Channels, add me to you Channel and send any media files and see miracle✨ also send /list to know all commands""",
-        
-        disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [InlineKeyboardButton("Owner", url="https://t.me/MadAsGhost")],
-                [InlineKeyboardButton("Support", url="https://t.me/TomenMain")]
-            ]
-        )
+        reply_markup=InlineKeyboardMarkup(buttons)
     )
